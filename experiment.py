2bac36ca1d704afe8f90654a03e41ab61e54bace
Milagro Teruel <milagro.teruel@gmail.com>
2017-02-17 17:01:15 -0300

    Finished Sampled Experiment and Skleanr Models

--- experiment.py
+++ experiment.py
@@ -1,3 +1,13 @@
+import os
+
+import logging
+
+import numpy
+import pandas
+
+logging.basicConfig(level=logging.INFO)
+
+from sklearn import metrics
 
 
 class Experiment(object):
@@ -20,24 +30,119 @@ class Experiment(object):
     def __init__(self, dataset, name=None, results_dirpath=None, config={}):
         self.dataset = dataset
         self.name = name
-        self.results_dirpath = results_dirpath
+        if results_dirpath is not None:
+            self.results_dirpath = results_dirpath
+        else:
+            self.results_dirpath = '.'
         self._read_config(config)
 
     def _read_config(self, config):
-        model_class = config.get('model', None)
-        if model_class is None or not isinstance(model_class, type):
+        self.model_class = config.get('model', None)
+        if self.model_class is None or not isinstance(self.model_class, type):
             raise ValueError('No model provided or model is not a class.')
-        model_params = config.get('model_params', {})
-        self.model = model_class(**model_params)
+        self.model_arguments = config.get('model_arguments', {})
 
-    def run(self):
-        """Executes the experiment."""
+    def _create_model(self):
+        self.model = self.model_class(self.dataset, **self.model_arguments)
+
+    def _save_predictions(self, predictions, partition_name):
+        if self.name is not None:
+            filename = '{}_predictions_{}.csv'.format(partition_name, self.name)
+        else:
+            filename = '{}_predictions.csv'.format(partition_name)
+        predictions = pandas.DataFrame(predictions, columns=['prediction'])
+        predictions['true'] = self.dataset.datasets[partition_name].labels
+        predictions.to_csv(os.path.join(self.results_dirpath, filename))
+
+    def _get_metrics(self, predictions, partition_name):
+        """Returns personalized metrics using predictions."""
+        metric_values = numpy.array(metrics.precision_recall_fscore_support(
+            self.dataset.datasets[partition_name].labels, predictions)).T
+        report = (
+            '\tPrecision\tRecall\tF1 Score\tSupport\n' +
+            '\n'.join(['Class {}\t'.format(index) + '\t'.join(
+                [str(value) for value in row])
+                for index, row in enumerate(metric_values)]))
+        logging.info('\n' + report)
+
+    def run(self, save_model=False, save_predictions=False):
+        """Executes the experiment.
+
+        Args:
+            save_model (bool): If true, saves the model in self.results_dirpath.
+            save_predictions (bool): If true, saves test predictions in
+                self.results_dirpath.
+        """
         # Train classifier
-        self.model.fit(self.dataset.datasets['train'].instances,)
+        self._create_model()
+        self.model.fit('train')
         # Save model
+        if save_model:
+            self.model.save_to_file(directory_name=self.results_dirpath,
+                                    name=self.name)
+        # Evaluate
+        predictions = self.model.predict('test')
+        self._get_metrics(predictions, partition_name='test')
+
         # Save the results
+        if save_predictions:
+            self._save_predictions(predictions, partition_name='test')
 
 
 class SampledExperiment(Experiment):
     """Abstraction to run the same experiment with a sampled dataset."""
-    pass
+
+    def _get_name_for_sample(self, sample):
+        if self.name is None:
+            return 'sample' + str(sample)
+        return self.name + '_sample' + str(sample)
+
+    def _save_predictions(self, predictions, partition_name, sample=0):
+        filename = '{}_predictions_{}.csv'.format(
+            partition_name, self._get_name_for_sample(sample))
+        predictions = pandas.DataFrame(predictions,
+                                       columns=['prediction', 'true'])
+        predictions.to_csv(os.path.join(self.results_dirpath, filename))
+
+    def _get_metrics(self, predictions, partition_name=None):
+        metric_values = []
+        for true, prediction in predictions:
+            metric_values.append(metrics.precision_recall_fscore_support(
+                true, prediction, average='micro'
+            )[:-1])
+        metric_values = numpy.array(metric_values)
+        report = ('\n\tPrecision\tRecall\tF1 Score\n' + 'mean\t' +
+            '\t'.join([str(x) for x in metric_values.mean(axis=0)]) +
+            '\nstd\t' + '\t'.join([str(x) for x in metric_values.std(axis=0)])
+        )
+        logging.info(report)
+
+    def run(self, save_model=False, save_predictions=False):
+        """Executes the experiment.
+
+        Args:
+            save_model (bool): If true, saves the trained models in
+                self.results_dirpath.
+            save_predictions (bool): If true, saves test predictions in
+                self.results_dirpath.
+        """
+        predictions = []
+        for sample in range(self.dataset.samples_num):
+            # Train classifier
+            self.dataset.set_current_sample(sample)
+            self._create_model()
+            self.model.fit('train')
+            # Save model
+            if save_model:
+                self.model.save_to_file(directory_name=self.results_dirpath,
+                                        name=self._get_name_for_sample(sample))
+            # Evaluate
+            predictions.append((self.dataset.datasets['test'].labels,
+                                self.model.predict('test')))
+
+            # Save the results
+            if save_predictions:
+                self._save_predictions(predictions[-1], partition_name='test',
+                                       sample=sample)
+
+        self._get_metrics(predictions)
---
+++ model.py
@@ -0,0 +1,72 @@
+import os
+
+import utils
+
+
+class BaseModel(object):
+    """Abstraction to represent trainable ML models using Dataset instances.
+
+    Attributes:
+        dataset: An instance of BaseDataset
+
+    Args:
+        dataset (:obj: BaseDataset): An instance of BaseDataset (or
+            subclass).
+        **kwargs: Additional arguments.
+    """
+
+    def __init__(self, dataset, **kwargs):
+        self.dataset = dataset
+
+    def fit(self, partition_name):
+        """Train the model with a dataset partition.
+
+        Args:
+            partition_name (str): The partition of the dataset to use for
+                fitting.
+        """
+        raise NotImplementedError
+
+    def predict(self, partition_name):
+        """Return the prediction over a dataset partition.
+
+        Args:
+            partition_name (str): The partition of the dataset to use for
+                prediction.
+        """
+        raise NotImplementedError
+
+    def save_to_file(self, directory_name, name=None):
+        """Save model to directory_name.
+
+        Args:
+            directory_name (string): Name of directory to save files.
+            name (string, optional): additional name to add into the dataset
+                files.
+        """
+        raise NotImplementedError
+
+
+class SkleanrModel(BaseModel):
+    """Wrapper around scikit-learn models compatible with BaseModel API."""
+
+    def __init__(self, dataset, model_class, sklearn_model_arguments={}):
+        super(SkleanrModel, self).__init__(dataset)
+        self.model = model_class(**sklearn_model_arguments)
+
+    def fit(self, partition_name):
+        self.model.fit(self.dataset.datasets[partition_name].instances,
+                       self.dataset.datasets[partition_name].labels)
+
+    def predict(self, partition_name):
+        return self.model.predict(
+            self.dataset.datasets[partition_name].instances)
+
+    def save_to_file(self, directory_name, name=None):
+        if name is not None:
+            filename = os.path.join(directory_name, '{}_model.p'.format(name))
+        else:
+            filename = os.path.join(directory_name, 'model.p')
+
+        utils.safe_mkdir(directory_name)
+        utils.pickle_to_file(self.model, filename)
